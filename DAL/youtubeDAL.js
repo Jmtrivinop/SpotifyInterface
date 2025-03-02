5438269232a25c6e6f312f46687f09642d3f386f
aflasso <andresfelipe.lasso@gmail.com>
2025-03-02 12:25:28 -0500

    youtube dal fix

--- DAL/youtubeDAL.js
+++ DAL/youtubeDAL.js
@@ -4,160 +4,123 @@ const API_KEY = process.env.API_KEY
 
 async function searchVideosByQuery(query) {
 
-    try {
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-            params: {
-                part: 'snippet',
-                q: query,
-                key: API_KEY
-            }
-        })
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+            part: 'snippet',
+            q: query,
+            key: API_KEY
+        }
+    })
 
-        console.log(response.data)
-        return response.data
-    } catch (error) {
-        console.error("Error al obtener los videos:", error.response.data);
-        return { success: false, message: 'Error obteniendo los videos', error: error.message };
-    }
+    console.log(response.data)
+    return response.data
 }
 
 async function searchVideoById(videoId) {
 
-    try {
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
-            params: {
-                part: 'snippet',
-                id: videoId,
-                key: API_KEY
-            }
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+        params: {
+            part: 'snippet',
+            id: videoId,
+            key: API_KEY
+        }
 
-        })
+    })
 
-        return response.data
-    } catch (error) {
-        
-        console.error('Error obteniendo el video', error)
-        return { success: false, message: 'Error obteniendo el video', error: error.message };
-
-    }
-
+    return response.data
 }
 
 async function getMyVideos(accesToken) {
-
-    try {
         
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
 
-            headers: {
-                authorization: `Bearer ${accesToken}`
-            },
-            params: {
+        headers: {
+            authorization: `Bearer ${accesToken}`
+        },
+        params: {
 
-                part: 'snippet',
-                forMine: true,
-                type: 'video'
+            part: 'snippet',
+            forMine: true,
+            type: 'video'
 
-            }
+        }
 
-        })
+    })
 
-        return response.data
-
-    } catch (error) {
-        
-        console.error('Error obteniendo los videos', error)
-        return { success: false, message: 'Error obteniendo los videos', error: error.message };
-
-    }
-
+    return response.data
 }
 
 async function getMySubscriptions(accesToken) {
-
-    try {
         
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/subscriptions', {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/subscriptions', {
 
-            headers: {
-                authorization: `Bearer ${accesToken}`
-            },
-            params: {
+        headers: {
+            authorization: `Bearer ${accesToken}`
+        },
+        params: {
 
-                part: 'snippet',
-                mine: true,
+            part: 'snippet',
+            mine: true,
 
-            }
+        }
 
-        })
+    })
 
-        return response.data
-
-    } catch (error) {
-        
-        console.error('Error obteniendo el video', error)
-        return { success: false, message: 'Error obteniendo las subscripciones', error: error.message };
-
-    }
-
+    return response.data
 }
 
 
 async function getMyPlaylist(accesToken) {
-    
-    try {
         
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/playlists', {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/playlists', {
 
-            headers: {
-                authorization: `Bearer ${accesToken}`
-            },
-            params: {
+        headers: {
+            authorization: `Bearer ${accesToken}`
+        },
+        params: {
 
-                part: 'snippet',
-                mine: true,
+            part: 'snippet',
+            mine: true,
 
-            }
+        }
 
-        })
+    })
 
-        return response.data
-
-    } catch (error) {
-        
-        console.error('Error obteniendo las playlist', error)
-        return { success: false, message: 'Error obteniendo las playlist', error: error.message };
-
-    }
+    return response.data
 
 }
 
 async function getTrendingVideos() {
-    
-    try {
         
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
 
-            params: {
+        params: {
 
-                part: 'snippet',
-                chart: 'mostPopular',
-                regionCode: 'CO',
-                key: API_KEY
+            part: 'snippet',
+            chart: 'mostPopular',
+            regionCode: 'CO',
+            key: API_KEY
 
-            }
+        }
 
-        })
+    })
 
-        return response.data
-
-    } catch (error) {
-        
-        console.error('Error obteniendo las tendencias', error)
-        return { success: false, message: 'Error obteniendo los videos en tendencia', error: error.message };
-
-    }
+    return response.data
 
 }
 
-module.exports = {searchVideosByQuery, searchVideoById, getMyVideos, getMySubscriptions, getMyPlaylist, getTrendingVideos}
+async function getChannelById(channelId) {
+    
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/channels', {
+        params: {
+            part: 'snippet',
+            id: channelId,
+            key: API_KEY
+        }
+    })
+
+    return response.data
+
+}
+
+module.exports = {searchVideosByQuery, searchVideoById, getMyVideos, getMySubscriptions, getMyPlaylist, getTrendingVideos, getChannelById}
